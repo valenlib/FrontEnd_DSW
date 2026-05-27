d3b08fb2c1d079cc525b283b17f9907cbfdfe66a
valenlib <valentinolibardi@gmail.com>
2026-05-27 17:17:24 -0300

    Búsqueda agregada al Home

--- frontend_react_app/src/pages/Home.tsx
+++ frontend_react_app/src/pages/Home.tsx
@@ -2,6 +2,8 @@ import "../styles/Home.css"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import Slider from "../components/Slider"
+import { Link, Route, Router } from "react-router-dom"
+import BusquedaVuelo from "../components/BusquedaVuelo"
 const Home: React.FC = () => {
   return (
     <>
@@ -10,7 +12,20 @@ const Home: React.FC = () => {
         <h1>Bienvenido a la página de inicio</h1>
         <p>Esta es la página principal de nuestra aplicación.</p>
       </div>
-      <Slider />
+      <div className="home-container">
+        <BusquedaVuelo />
+      </div>
+      <div className="slider-container">
+        <Slider />
+      </div>    
+        
+      <div className="home-container">
+        <h2>Explora nuestras ofertas</h2>
+        <p>Descubre las mejores ofertas de vuelos y destinos.</p>
+        <Link className="explore-button" to="/vuelos">
+          Explorar Ofertas
+        </Link>
+      </div>
         <Footer />
     </>
   )
